cbf7936f7c39cae6b9ec97efcb5ba82cdf53cd71
apally <adrien.pally@orange.com>
2021-09-17 16:11:15 +0200

    add UI to see crypt/decrypt data

--- app/src/main/java/com/example/crypttoken/MainActivity.kt
+++ app/src/main/java/com/example/crypttoken/MainActivity.kt
@@ -19,30 +19,56 @@ import org.jose4j.keys.EllipticCurves
 class MainActivity : AppCompatActivity() {
     private val TAG = MainActivity::class.qualifiedName
     private lateinit var binding: ActivityMainBinding
+    private val applicationDataClaimName = "applicationData"
+    private val senderKeys: EllipticCurveJsonWebKey = EcJwkGenerator.generateJwk(EllipticCurves.P256)
+    private val receiverKeys: EllipticCurveJsonWebKey = EcJwkGenerator.generateJwk(EllipticCurves.P256)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //init keys
+        senderKeys.keyId = "sender's key"
+        receiverKeys.keyId = "receiver's key"
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.fab.setOnClickListener { view ->
-            cryptData()
+        binding.switchCrypto.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                cryptData(binding.entryText.text.toString())
+            } else {
+                decryptData()
+            }
         }
     }
 
-    private fun cryptData() {
-        val senderKeys: EllipticCurveJsonWebKey = EcJwkGenerator.generateJwk(EllipticCurves.P256)
-        senderKeys.keyId = "sender's key"
+    private fun decryptData() {
+        // client
 
-        val receiverKeys: EllipticCurveJsonWebKey = EcJwkGenerator.generateJwk(EllipticCurves.P256)
-        receiverKeys.keyId = "receiver's key"
+        val jwtConsumer = JwtConsumerBuilder()
+            .setDecryptionKey(receiverKeys.privateKey) // decrypt with the receiver's private key
+            .setVerificationKey(senderKeys.publicKey) // verify the signature with the sender's public key
+            .build()
 
-        val applicationDataClaimName = "applicationData"
+        try {
+            val jwtClaims = jwtConsumer.processToClaims(binding.resultView.text.toString())
+            val applicationData = jwtClaims.getClaimValueAsString(applicationDataClaimName)
+            binding.resultView.text = applicationData
+            Log.i(
+                TAG,
+                "decrypted application data : $applicationData"
+            ) // application data decrypted and signature validated
+        } catch (e: InvalidJwtException) {
+            // InvalidJwtException will be thrown, if the JWT failed processing or validation in anyway.
+            // Hopefully with meaningful explanations(s) about what went wrong.
+            Log.i(TAG, "Invalid JWT! $e")
+        }
+    }
 
+    private fun cryptData(text: String) {
         val claims = JwtClaims()
 
-        claims.setClaim(applicationDataClaimName, "tokenV2 data") // applicationData
+        claims.setClaim(applicationDataClaimName, text) // applicationData
 
         val jws = JsonWebSignature()
         jws.payload = claims.toJson()
@@ -66,24 +92,9 @@ class MainActivity : AppCompatActivity() {
         jwe.contentTypeHeaderValue = "JWT"
         jwe.payload = innerJwt
         val jwt = jwe.compactSerialization // applicationData + signature encrypted
-
+        binding.resultView.text = jwt
         Log.i(TAG, "crypted application data: $jwt")
 
-        // client
 
-        val jwtConsumer = JwtConsumerBuilder()
-            .setDecryptionKey(receiverKeys.privateKey) // decrypt with the receiver's private key
-            .setVerificationKey(senderKeys.publicKey) // verify the signature with the sender's public key
-            .build()
-
-        try {
-            val jwtClaims = jwtConsumer.processToClaims(jwt)
-            val applicationData = jwtClaims.getClaimValueAsString(applicationDataClaimName)
-            Log.i(TAG, "decrypted application data : $applicationData") // application data decrypted and signature validated
-        } catch (e: InvalidJwtException) {
-            // InvalidJwtException will be thrown, if the JWT failed processing or validation in anyway.
-            // Hopefully with meaningful explanations(s) about what went wrong.
-            Log.i(TAG, "Invalid JWT! $e")
-        }
     }
 }
