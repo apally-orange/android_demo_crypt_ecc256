8bca871bfa94962798973088c45d113e8a8ac28e
apally <adrien.pally@orange.com>
2021-09-17 14:47:02 +0200

    import initial

---
+++ app/src/main/java/com/example/crypttoken/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.crypttoken
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.crypttoken.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fab.setOnClickListener { view ->
+           cryptData()
+        }
+    }
+
+    private fun cryptData(){
+
+    }
+}
